b9f5ec2a8785e5703edd6b4a73ddba0de64eb346
Oscar Mojica <o_mojical@hotmail.com>
2022-01-12 15:09:37 -0300

    Update test_subprocess.py

--- test_subprocess.py
+++ test_subprocess.py
@@ -1,16 +1,21 @@
 # -*- coding: utf-8 -*-
 
 import subprocess as sp
-import os
 
 
 def readGitVersion():
-    proc = sp.Popen("git --version", cwd=os.getcwd(),
-    				 shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
-    output, error = proc.communicate()
-    if proc.returncode != 0:
-        raise Exception("git command call failed %d %s %s" % (proc.returncode, output, error))
-    return output.splitlines()[0].strip()
+    try:
+        proc = sp.check_output(('git', 'describe', '--long',
+                                 '--match', 'v[0-9]*.*'),
+                               stderr=sp.STDOUT)
+    except sp.CalledProcessError as error:
+        errorMessage = ">>> Error while executing:\n"\
+                       + command\
+                       + "\n>>> Returned with error:\n"\
+                       + str(error.output)
+        print("Error: " + errorMessage)
+
+    return proc.splitlines()[0].strip()
 
 if __name__ == '__main__':
     print(readGitVersion())
