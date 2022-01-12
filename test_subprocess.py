1829e2ce2480ad59f438c57517d38ffcf58cac7b
Oscar Mojica <o_mojical@hotmail.com>
2022-01-12 16:16:13 -0300

    Update test_subprocess.py

--- test_subprocess.py
+++ test_subprocess.py
@@ -6,16 +6,25 @@ import subprocess as sp
 def readGitVersion():
     try:
         proc = sp.check_output(('git', 'describe', '--long',
-                                 '--match', 'v[0-9]*.*'),
+                               '--match', 'v[0-9]*.*'),
                                stderr=sp.STDOUT).splitlines()[0].strip()
+        return proc
     except sp.CalledProcessError as error:
         errorMessage = ">>> Error while executing:\n"\
                        + 'git describe'\
                        + "\n>>> Returned with error:\n"\
                        + str(error.output)
         print("Error: " + errorMessage)
+        return None
 
-    return proc
+
+def git_describe_run():
+    proc = sp.run(['git', 'describe', '--long', '--match', 'v[0-9]*.*'],
+                  capture_output=True, text=True,
+                  check=False)
+    print("stdout:", proc.stdout)
+    print("stderr:", proc.stderr)
 
 if __name__ == '__main__':
-    print(readGitVersion())
+    #print(readGitVersion())
+    git_describe_run()
