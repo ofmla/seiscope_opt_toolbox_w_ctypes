dafcb401d4ce86e1938ec4118dab452ed2556f64
Oscar Mojica <o_mojical@hotmail.com>
2022-01-12 14:13:03 -0300

    Update test_subprocess.py

--- test_subprocess.py
+++ test_subprocess.py
@@ -4,11 +4,11 @@ import subprocess as sp
 
 
 def readGitVersion():
-    proc = sp.Popen("git describe --long --match v[0-9]*.*",
+    proc = sp.Popen("ls tests/",
     				 shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
     output, error = proc.communicate()
     if proc.returncode != 0:
-        raise Exception("git command call failed %d %s %s" % (proc.returncode, output, error))
+        raise Exception("ls command call failed %d %s %s" % (proc.returncode, output, error))
     return output.splitlines()[0].strip()
 
 if __name__ == '__main__':
