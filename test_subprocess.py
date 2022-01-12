4b0114ec8c03666d5261e3dcf88abf9012f427ef
Oscar Mojica <o_mojical@hotmail.com>
2022-01-12 16:45:44 -0300

    Update test_subprocess.py

--- test_subprocess.py
+++ test_subprocess.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import subprocess as sp
-
+import os
+import pathlib
 
 def readGitVersion():
     try:
@@ -21,10 +22,20 @@ def readGitVersion():
 def git_describe_run():
     proc = sp.run(['git', 'describe', '--long', '--match', 'v[0-9]*.*'],
                   capture_output=True, text=True,
-                  check=False)
+                  check=True, env={"PATH": "/usr/bin"})
     print("stdout:", proc.stdout)
     print("stderr:", proc.stderr)
+    print(sp.check_output(["git", "describe", "--always"]).strip().decode())
+
+def get_git_revision(base_path):
+    git_dir = pathlib.Path(base_path) / '.git'
+    with (git_dir / 'HEAD').open('r') as head:
+        ref = head.readline().split(' ')[-1].strip()
+
+    with (git_dir / ref).open('r') as git_hash:
+        return git_hash.readline().strip()
 
 if __name__ == '__main__':
     #print(readGitVersion())
     git_describe_run()
+    print(get_git_revision(os.path.dirname(__file__)))
