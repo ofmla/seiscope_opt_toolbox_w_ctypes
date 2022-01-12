06a8415b6d1f3ef606e794302e0cd68a224291e4
Oscar Mojica <o_mojical@hotmail.com>
2022-01-12 12:39:46 -0300

    Update test_subprocess.py

--- test_subprocess.py
+++ test_subprocess.py
@@ -9,7 +9,7 @@ def readGitVersion():
                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output, error = proc.communicate()
     if proc.returncode != 0:
-        raise Exception("git command call failed %d %s %s" % (process.returncode, output, error))
+        raise Exception("git command call failed %d %s %s" % (proc.returncode, output, error))
     return output.splitlines()[0].strip()
 
 if __name__ == '__main__':
