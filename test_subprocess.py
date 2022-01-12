f6ec56c20c25d6773b7b14899127ed3dc097c14b
ofmla <o_mojical@hotmail.com>
2022-01-12 12:37:10 -0300

    one more test in CI.yml

---
+++ test_subprocess.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import subprocess
+
+
+def readGitVersion():
+    proc = subprocess.Popen(('git', 'describe', '--long',
+                             '--match', 'v[0-9]*.*'),
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, error = proc.communicate()
+    if proc.returncode != 0:
+        raise Exception("git command call failed %d %s %s" % (process.returncode, output, error))
+    return output.splitlines()[0].strip()
+
+if __name__ == '__main__':
+    print(readGitVersion())
