3990359868c2b8a1f491f8b7cbf11333f1f80fa0
Oscar Mojica <o_mojical@hotmail.com>
2022-01-12 14:00:36 -0300

    Update test_subprocess.py

--- test_subprocess.py
+++ test_subprocess.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 
-import subprocess
+import subprocess as sp
 
 
 def readGitVersion():
-    proc = subprocess.Popen(('git', 'describe', '--long',
-                             '--match', 'v[0-9]*.*'),
-                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    proc = sp.Popen("git describe --long --match v[0-9]*.*",
+    				 shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
     output, error = proc.communicate()
     if proc.returncode != 0:
-        raise Exception("git command call failed %d %s %s" % (proc.returncode, output, error))
+        raise Exception("git command call failed %d %s %s" % (process.returncode, output, error))
     return output.splitlines()[0].strip()
 
 if __name__ == '__main__':
