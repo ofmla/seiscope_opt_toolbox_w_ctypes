93ee9e0b5eed6956ee42ef23755bea22922a0a53
Oscar Mojica <o_mojical@hotmail.com>
2022-09-27 13:45:17 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 import os
+import site
 import sys
 from skbuild import setup  # This line replaces 'from setuptools import setup'
 sys.path.insert(0, os.path.dirname(__file__))
 import versioneer
 
+
+install_dir = ''.join(site.getsitepackages())
+install_name_dir = install_dir+'/sotb_wrapper'
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -21,7 +25,7 @@ configuration = {
     'author': "Oscar Mojica",
     'author_email': 'o_mojical@hotmail.com',
     'package_data': {'sotb-wrapper': ['libsotb.so', 'test_*'], },
-    'cmake_args': ["-DSKBUILD=ON"],
+    'cmake_args': ["-DSKBUILD=ON", "-DCMAKE_INSTALL_NAME_DIR="+install_name_dir],
     'license': 'MIT',
     'install_requires': ['numpy>=1.20'],
     'setup_requires': ['versioneer'],
